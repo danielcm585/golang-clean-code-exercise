1df6c8ba3c196f8e02bf1c2f2ea7bb10b0ab75bd
danielcm585 <74773920+danielcm585@users.noreply.github.com>
2021-08-08 09:32:20 +0700

    Add files via upload

---
+++ models/transaction.go
@@ -0,0 +1,19 @@
+package models
+
+type transactions struct {
+	data map[int]int
+}
+
+func NewTransactions(d []int) Transactions {
+	data := make(map[int]int)
+
+	for i, v := range d {
+		data[i] = v;
+	}
+
+	return &transactions{data: data}
+}
+
+func (t *transactions) Get(idx int) int {
+	return t.data[idx]
+}
